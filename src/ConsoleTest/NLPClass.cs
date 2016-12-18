374ce232b567bef99b73cbec81b7be724d0ddb8d
Andrei Kostyrin <kostyrin2@gmail.com>
2016-12-18 18:19:30 +0200

    ConsoleTest and projects

---
+++ src/ConsoleTest/NLPClass.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using OpenNLP.Tools.SentenceDetect;
+
+namespace OpenNLP.ConsoleApp2
+{
+    public class NLPClass
+    {
+        private MaximumEntropySentenceDetector _sentenceDetector;
+        private readonly string _modelPath;
+
+        public NLPClass()
+        {
+            _modelPath = Directory.GetParent(Directory.GetCurrentDirectory()) + "\\Resources\\Models\\";
+            
+        }
+
+        public string[] Split(string paragraph)
+        {
+            return SplitSentences(paragraph);
+        }
+
+        private string[] SplitSentences(string paragraph)
+        {
+            if (_sentenceDetector == null)
+            {
+                _sentenceDetector = new EnglishMaximumEntropySentenceDetector(_modelPath + "EnglishSD.nbin");
+            }
+
+            return _sentenceDetector.SentenceDetect(paragraph);
+        }
+    }
+}
---
+++ src/ConsoleTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OpenNLP.ConsoleApp2;
+
+namespace ConsoleTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string str = ".NET Framework (pronounced dot net) is a software framework developed by Microsoft that runs primarily on Microsoft Windows. It includes a large class library known as Framework Class Library (FCL) and provides language interoperability (each language can use code written in other languages) across several programming languages. Programs written for .NET Framework execute in a software environment (as contrasted to hardware environment) known as Common Language Runtime (CLR), an application virtual machine that provides services such as security, memory management, and exception handling. (As such, computer code written using .NET Framework is called 'managed code'.) FCL and CLR together constitute .NET Framework.";
+            var arr = new NLPClass().Split(str);
+            foreach (var s in arr)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+        }
+    }
+}
